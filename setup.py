6468b358cc527c24b0e1fd833e190bbc38a65b37
fredgj <f.gjertsen@gmail.com>
2015-06-24 08:21:46 +0200

    updated documentation

--- setup.py
+++ setup.py
@@ -3,43 +3,42 @@ Welcome to GjertsenTweet's documentation!
 =========================================
 
 **GjertsenTweet** is a twitter client built for the command line.
-It has been built with npyscreen to create a simple text-based user interface.
+It has been built with npyscreen which is based on curses to create a simple 
+text-based user interface.
 
-It lets you read the tweets from your feed, search and post tweets.
+It lets you read the tweets from your feed, search for tweets and post tweets.
+
+.. image:: http://i.imgur.com/t3bofl5.png
 
 Usage
 -----
-When launched for the first you will be asked to open a link in your browser
-in order to approve the app and login.
+Run *gjertsentweet* in your terminal to fire it up. There's no command-line arguments.
+When launched for the first time you will be asked to open a link in your browser
+in order to approve the app. You will be given a pin from twitter which lets 
+you log into your account from GjertsenTweet.
 
-When logged in, you can simply use the keys or enter to move around.
-Note that once you have moved down to feed, you can't move back up with
-the keys. You have to either press cancel or ok to go back to the top.
-
-When cancel is pressed it will ask you if you wanna quit or not. If you say no,
-the cursor will jump back to the top.
-
-When you have entered something under 'What's happening', and press ok
-it will first check if your tweet have a valid size (>= 140 characters).
-If it's more than 140 characters it will delete what you have written and jump
-back to the top.
-
-If the tweet has a valid length, it will ask you wether you wanna post this or
-not.
-
-If you have entered somehting to search it will simply search for this and
-update you feed without asking.
+When logged in, you can simply use the arrow keys or enter to move around.
+Note that once you have moved down to feed, you can't use the arrow key to
+move the cursor back to the tweet/search fields. To do this you will have to
+press one of the buttons.
 
 The twitter feed will be updated on the fly.
 
 Note that you have to press enter when the dialog boxes pops up in order
-to move the cursor to the button/buttons
+to move the cursor down to the button/buttons
+
+Platform
+--------
+This was originally written to be run on unix like operating systems, but
+it's also possible to run on windows, all you need to do is to install curses 
+for windows from here http://www.lfd.uci.edu/~gohlke/pythonlibs/ if you haven't 
+already done it  
 """
 
 from setuptools import setup
 
 setup(name='GjertsenTweet',
-      version='0.3.5',
+      version='0.3.7',
       description='A simple twitter client for the terminal',
       long_description = __doc__,
       author='Fredrik Gjertsen',
@@ -51,7 +50,8 @@ setup(name='GjertsenTweet',
                         'twitter==1.17.0'],
       entry_points={
           'console_scripts':
-            ['gjertsentweet=GjertsenTweet.client:main'],
+            ['gjertsentweet=GjertsenTweet.client:main',
+             'GjertsenTweet=GjertsenTweet.client:main']
           },
       classifiers=['Development Status :: 4 - Beta',
                    'Environment :: Console',
