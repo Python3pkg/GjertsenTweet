e5069c8a3bda68e38bcddaa192a2853c8d7557b0
fredgj <f.gjertsen@gmail.com>
2015-08-13 01:15:09 +0200

    cleaned up

--- setup.py
+++ setup.py
@@ -1,47 +1,13 @@
-"""
-Welcome to GjertsenTweet's documentation!
-=========================================
-
-**GjertsenTweet** is a twitter client built for the command line.
-It has been built with npyscreen which is based on curses to create a simple 
-text-based user interface.
-
-It lets you read the tweets from your feed, search for tweets and post tweets.
-
-.. image:: http://i.imgur.com/t3bofl5.png
-
-Usage
------
-Run *gjertsentweet* in your terminal to fire it up. There's no command-line arguments.
-When launched for the first time you will be asked to open a link in your browser
-in order to approve the app. You will be given a pin from twitter which lets 
-you log into your account from GjertsenTweet.
-
-When logged in, you can simply use the arrow keys or enter to move around.
-Note that once you have moved down to feed, you can't use the arrow key to
-move the cursor back to the tweet/search fields. To do this you will have to
-press one of the buttons.
-
-The twitter feed will be updated on the fly.
-
-Note that you have to press enter when the dialog boxes pops up in order
-to move the cursor down to the button/buttons
-
-Platform
---------
-This was originally written to be run on unix like operating systems, but
-it's also possible to run on windows, all you need to do is to install curses 
-for windows from here http://www.lfd.uci.edu/~gohlke/pythonlibs/ if you haven't 
-already done it  
-"""
-
 __author__ = 'Fredrik Gjertsen'
+__doc__ = open('readme.rst')
+__description__ = 'A simple twitter client for the command line'
+__version__ = '0.4.9'
 
 from setuptools import setup
 
 setup(name='GjertsenTweet',
-      version='0.4.7',
-      description='A simple twitter client for the command line',
+      version=__version__,
+      description=__description__,
       long_description = __doc__,
       author=__author__,
       author_email='f.gjertsen@gmail.com',
@@ -49,7 +15,8 @@ setup(name='GjertsenTweet',
       packages=['GjertsenTweet'],
       license='GNU General Public License',
       install_requires=['npyscreen==4.9.1',
-                        'twitter==1.17.0'],
+                        'twitter==1.17.0',
+                        'dict-digger==0.2.1'],
       entry_points={
           'console_scripts':
             ['gjertsentweet=GjertsenTweet.client:main',
